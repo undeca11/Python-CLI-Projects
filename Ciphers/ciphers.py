bf8ed28751e09cb5394153b22254336fd88337dd
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-04-20 16:35:00 -0300

    Added upper case support

--- Ciphers/ciphers.py
+++ Ciphers/ciphers.py
@@ -2,19 +2,26 @@ import string
 
 def main():
     x = int(input("This is a encryption assisting tool made as a Python Project\n" + "Do you wish to (1)encrypt or (2)decrypt? "))
-    message = input("Please write your message: ")
-    n = int(input("Write the shift/offset: ")) % 26
+    message = input("Please write your message= ")
+    n = int(input("Write the shift/offset= ")) % 26
     match x:
         case 1:
             print(caesar(message, n))
         case 2:
             print(caesar(message, -n))
 
-def caesar(message: str, n: int):
-    alphabet = string.ascii_lowercase
+def caesar(message= str, n= int):
+    alphabetLower = string.ascii_lowercase
+    alphabetUpper = string.ascii_uppercase
     encryptedMessage = []
     for i in range(len(message)):
-        if alphabet.count(message[i]) != 0:
+        if alphabetLower.count(message[i]) != 0:
+            alphabet = alphabetLower
+        elif alphabetUpper.count(message[i]) != 0:
+            alphabet = alphabetUpper
+        else:
+            alphabet = False
+        if alphabet != False:
             alphabetLetter = alphabet.index(message[i])
             encryptedMessage.insert(i, alphabet[alphabetLetter+n])
         else: encryptedMessage.insert(i, message[i])
