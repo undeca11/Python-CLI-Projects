34d398169891a624f542bdf0aaeae3e83e231c68
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-04-19 22:08:57 -0300

    Update to encryption code, no need for separate list for encypted alphabet now

--- Ciphers/ciphers.py
+++ Ciphers/ciphers.py
@@ -2,7 +2,7 @@ import string
 
 def main():
     x = int(input("This is a encryption assisting tool made as a Python Project\n" + "Do you wish to (1)encrypt or (2)decrypt? "))
-    message = input("Please write the message you wish to encrypt or decrypt: ")
+    message = input("Please write your message: ")
     n = int(input("Write the shift/offset: "))
     match x:
         case 1:
@@ -14,18 +14,11 @@ def caesar(message: str, n: int):
     if n > 26:
         n -= 26
     alphabet = string.ascii_lowercase
-    cryptAlphabet = ["-" for i in alphabet]
-    for i in range(len(cryptAlphabet)):
-        new_i = i + n
-        if new_i > 25:
-            new_i -= 26
-        cryptAlphabet[i] = alphabet[new_i]
     encryptedMessage = []
     for i in range(len(message)):
         if alphabet.count(message[i]) != 0:
             alphabetLetter = alphabet.index(message[i])
-            cryptLetter = cryptAlphabet[alphabetLetter]
-            encryptedMessage.insert(i, cryptLetter)
+            encryptedMessage.insert(i, alphabet[alphabetLetter+n])
         else: encryptedMessage.insert(i, message[i])
     return "".join(encryptedMessage)
 
