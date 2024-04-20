9e868715fc267afc5534e3f461a1af997f955860
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-04-19 22:31:49 -0300

    Removed unnecessary func

--- Ciphers/ciphers.py
+++ Ciphers/ciphers.py
@@ -3,16 +3,14 @@ import string
 def main():
     x = int(input("This is a encryption assisting tool made as a Python Project\n" + "Do you wish to (1)encrypt or (2)decrypt? "))
     message = input("Please write your message: ")
-    n = int(input("Write the shift/offset: "))
+    n = int(input("Write the shift/offset: ")) % 26
     match x:
         case 1:
             print(caesar(message, n))
         case 2:
-            print(caesarDecode(message, n))
+            print(caesar(message, -n))
 
 def caesar(message: str, n: int):
-    if n > 26:
-        n -= 26
     alphabet = string.ascii_lowercase
     encryptedMessage = []
     for i in range(len(message)):
@@ -21,10 +19,5 @@ def caesar(message: str, n: int):
             encryptedMessage.insert(i, alphabet[alphabetLetter+n])
         else: encryptedMessage.insert(i, message[i])
     return "".join(encryptedMessage)
-
-def caesarDecode(message: str, n: int):
-    if n > 26:
-        n -= 26
-    return(caesar(message, -n))
 
 main()
