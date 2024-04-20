fe68d25c7dc632d6b77b5e62ce3b9f2de50bfc08
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-04-20 18:55:00 -0300

    Added morse code decryption

--- Ciphers/ciphers.py
+++ Ciphers/ciphers.py
@@ -4,21 +4,13 @@ def main():
     x = int(input("This is a encryption assisting tool made as a Python Project\n" + "Do you wish to (1)encrypt or (2)decrypt? "))
     message = input("Please write your message: ")
     cipher = int(input("What cipher do you wish to use? (1)Caesar Cipher (2)Caesar Box (3)Morse Code "))
-    match x:
+    match cipher:
         case 1:
-            match cipher:
-                case 1:
-                    n = int(input("Write the shift/offset= ")) % 26
-                    caesar(message, n)
-                case 3:
-                    morse(message)
-        case 2:
-            match cipher:
-                case 1:
-                    n = int(input("Write the shift/offset= ")) % 26
-                    caesar(message, -n)
-                case 3:
-                    morse(message)
+            n = int(input("Write the shift/offset= ")) % 26
+            n = n if x == 1 else -n
+            caesar(message, n)
+        case 3:
+            morse(message, x)
 
 def caesar(message= str, n= int):
     alphabetLower = string.ascii_lowercase
@@ -34,7 +26,7 @@ def caesar(message= str, n= int):
         else: encryptedMessage.insert(i, message[i])
     return print("".join(encryptedMessage))
 
-def morse(message= str):
+def morse(message= str, x= int):
     message = message.lower()
     morseNum = {
     "0" : "-----",
@@ -77,17 +69,34 @@ def morse(message= str):
     "z": "--.."
 }
     encryptedMessage = []
-    for i in range(len(message)):
-        if message[i] in morseNum:
-            insert = morseNum[message[i]]
-            encryptedMessage.insert(i, insert)
-        elif message[i] in morseAlphabet:
-            insert = morseAlphabet[message[i]]
-            encryptedMessage.insert(i, insert)
-        elif message[i] == " ":
-            encryptedMessage.insert(i, "/")
-        else: 
-            encryptedMessage.insert(i, message[i])
-    return print(" ".join(encryptedMessage))
+    match x:
+        case 1:
+            for i in range(len(message)):
+                if message[i] in morseNum:
+                    insert = morseNum[message[i]]
+                    encryptedMessage.insert(i, insert)
+                elif message[i] in morseAlphabet:
+                    insert = morseAlphabet[message[i]]
+                    encryptedMessage.insert(i, insert)
+                elif message[i] == " ":
+                    encryptedMessage.insert(i, "/")
+                else: 
+                    encryptedMessage.insert(i, message[i])
+            return print(" ".join(encryptedMessage))
+        case 2:
+            encryptedMessage = message.split(" ")
+            message = []
+            for i in range(len(encryptedMessage)):
+                if encryptedMessage[i] in morseNum.values():
+                    insert = list(morseNum.keys())[list(morseNum.values()).index(encryptedMessage[i])]
+                    message.insert(i, insert)
+                elif encryptedMessage[i] in morseAlphabet.values():
+                    insert = list(morseAlphabet.keys())[list(morseAlphabet.values()).index(encryptedMessage[i])]
+                    message.insert(i, insert)
+                elif encryptedMessage[i] == "/":
+                    message.insert(i, " ")
+                else:
+                    message.insert(i, encryptedMessage[i])
+            return print("".join(message))
 
 main()
