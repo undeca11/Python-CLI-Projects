71c3d55f0c70d8ebd871388fee6dca4369ac46ef
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-05-29 15:50:02 -0300

    Criptografador fernet usando cryptography

--- Encrypter/fernetEncrypter.py
+++ Encrypter/fernetEncrypter.py
@@ -1,19 +1,18 @@
-import os
-import pathlib
-from cryptography.fernet import Fernet, InvalidToken
+from os import path, urandom
+from pathlib import Path
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from base64 import urlsafe_b64encode
-from secrets import token_hex
 
 class fernetEncrypter:
     def __init__(self, password, filepath: str) -> None:
         self.filepath = filepath
         self.password = password.encode()
-        self.saltFilepath = os.path.join(os.path.dirname(__file__), 'salt.fernet')
-        self.p = pathlib.Path(self.saltFilepath)
-        if os.path.exists(self.saltFilepath) != True:
-            self.p.write_bytes(os.urandom(16))
+        self.saltFilepath = path.join(path.dirname(__file__), 'salt.fernet')
+        self.p = Path(self.saltFilepath)
+        if path.exists(self.saltFilepath) != True:
+            self.p.write_bytes(urandom(16))
         self.salt = self.p.read_bytes()
         self.kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
@@ -25,23 +24,23 @@ class fernetEncrypter:
         self.f = Fernet(self.key)
     
     def encrypt(self):
-        self.filename, self.extension = os.path.splitext(self.filepath)
+        self.filename, self.extension = path.splitext(self.filepath)
         self.extension = self.extension.encode()
-        self.p = pathlib.Path(self.filepath)
+        self.p = Path(self.filepath)
         self.data = self.p.read_bytes()
         self.data = self.data + self.extension
         self.p.write_bytes(self.f.encrypt(self.data))
-        self.target = pathlib.Path(self.filepath.replace(self.extension.decode(),'.fernet'))
+        self.target = Path(self.filepath.replace(self.extension.decode(),'.fernet'))
         self.p.rename(self.target)
         self.filepath = self.target
 
     def decrypt(self):
-        self.p = pathlib.Path(self.filepath)
+        self.p = Path(self.filepath)
         self.data = self.p.read_bytes()
         self.data = self.f.decrypt(self.data).decode()
         self.oldExtension = self.data[self.data.rfind('.'):]
         self.data = self.data[:self.data.rfind('.')].encode()
         self.p.write_bytes(self.data)
-        self.target = pathlib.Path(str(self.filepath).replace('.fernet',self.oldExtension))
+        self.target = Path(str(self.filepath).replace('.fernet',self.oldExtension))
         self.p.rename(self.target)
         self.filepath = self.target
