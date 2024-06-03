1ee8b2f7c200386c4af9105f6bc40413c59bd34a
Gabriel Moraes <gabrielmoraesrivero@gmail.com>
2024-06-03 20:09:29 -0300

    Legibilidade

--- Encrypter/fernetEncrypter.py
+++ Encrypter/fernetEncrypter.py
@@ -5,13 +5,13 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from base64 import urlsafe_b64encode
 
-class fernetEncrypter:
-    def __init__(self, password: bytes, filepath: Path, saltpath = path.join(path.dirname(__file__), 'salt.fernet')) -> None:
-        self.filepath = filepath
+class FernetEncrypter:
+    def __init__(self, password: bytes, filepath: Path, salt_path = path.join(path.dirname(__file__), 'salt.fernet')) -> None:
         self.password = password
-        self.saltpath = saltpath
-        self.p = Path(self.saltpath)
-        if path.exists(self.saltpath) != True:
+        self.filepath = filepath
+        self.salt_path = salt_path
+        self.p = Path(self.salt_path)
+        if path.exists(self.salt_path) != True:
             self.p.write_bytes(urandom(16))
         self.salt = self.p.read_bytes()
         self.kdf = PBKDF2HMAC(
@@ -42,9 +42,9 @@ class fernetEncrypter:
         except InvalidToken:
             print("InvalidToken Error")
         else:
-            self.oldExtension = self.data[self.data.rfind('.'):]
+            self._old_extension = self.data[self.data.rfind('.'):]
             self.data = self.data[:self.data.rfind('.')].encode()
             self.p.write_bytes(self.data)
-            self.target = Path(str(self.filepath).replace('.fernet',self.oldExtension))
+            self.target = Path(str(self.filepath).replace('.fernet',self._old_extension))
             self.p.rename(self.target)
             self.filepath = self.target
--- Encrypter/fileEncrypter.py
+++ Encrypter/fileEncrypter.py
@@ -1,18 +1,23 @@
-import fernetEncrypter as fe
+import fernet_encrypter as fe
 
 path = input('Digite o caminho do arquivo: ').replace('\\','/')
 pas = input('Qual a senha: ').encode()
 mode = input('Deseja (1) criptografar ou (2) Descriptografar: ')
 
-cript = fe.fernetEncrypter(pas, path)
+cript = fe.FernetEncrypter(pas, path)
 
 while True:
-    match int(mode):
-        case 1:
-            cript.encrypt()
-            quit()
-        case 2:
-            cript.decrypt()
-            quit()
-        case _:
-            mode = input('Deseja (1) criptografar ou (2) Descriptografar: ')
+    try:
+        mode = input('Deseja (1) criptografar, (2) Descriptografar ou (3) Sair do programa: ')
+        match int(mode):
+            case 1:
+                print("Opção 1")
+            case 2:
+                print("Opção 2")
+            case 3:
+                print('\nPrograma encerrado!')
+                quit()
+            case _:
+                raise ValueError
+    except ValueError:
+        print('Por favor, digite algo válido!')
